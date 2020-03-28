ae133e0c9ad8e972b29e9b9f932f3b9a2168175d
William Tang <williamtang903@gmail.com>
2020-03-28 16:32:56 -0400

    added twilio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from "react-google-maps";
-import {myjson} from "./data/hospital.js";
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import { myjson } from "./data/hospital.js";
 
 function Map() {
+  console.log("id ", process.env.REACT_APP_GOOGLE_KEY);
   const [selectedPark, setSelectedPark] = useState(null);
-  
+
   return (
-    <GoogleMap 
-      defaultZoom={10} 
-      defaultCenter={{ lat: 34.052235, lng: -118.243683}}
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 34.052235, lng: -118.243683 }}
     >
       {myjson.map(medical => (
-        <Marker 
-          key={medical.OBJECTID} 
+        <Marker
+          key={medical.OBJECTID}
           position={{
-          lat: medical.latitude,
-          lng: medical.longitude
+            lat: medical.latitude,
+            lng: medical.longitude
           }}
           onClick={() => {
             setSelectedPark(medical);
@@ -28,8 +35,8 @@ function Map() {
       {selectedPark && (
         <InfoWindow
           position={{
-          lat: selectedPark.latitude,
-          lng: selectedPark.longitude
+            lat: selectedPark.latitude,
+            lng: selectedPark.longitude
           }}
           onCloseClick={() => {
             setSelectedPark(null);
@@ -38,7 +45,7 @@ function Map() {
           <div>park details</div>
         </InfoWindow>
       )}
-    </GoogleMap> 
+    </GoogleMap>
   );
 }
 
@@ -46,15 +53,14 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
-      <WrappedMap 
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <WrappedMap
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=
           ${process.env.REACT_APP_GOOGLE_KEY}`}
-        loadingElement={<div style={{height: '100%'}} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
       />
     </div>
-  )
+  );
 }
-
