f269e9bb513b9b7e68f59364169017022f28f63f
William Tang <williamtang903@gmail.com>
2020-03-29 12:14:00 -0400

    final push

--- src/components/pages/FavoritePage.js
+++ src/components/pages/FavoritePage.js
@@ -1,16 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "../../App.css";
-import { GalleryCard } from 'tabler-react'
-import zion from './zion.jpg'
-import cvs from './cvs.jpg'
-import mini from './mini.jpg'
-import local from './local.jpg'
-import pen from './pen.jpg'
+import { GalleryCard } from "tabler-react";
+import zion from "./zion.jpg";
+import cvs from "./cvs.jpg";
+import mini from "./mini.jpg";
+import local from "./local.jpg";
+import pen from "./pen.jpg";
+import firebase from "firebase";
+import { makeid } from "./helpers";
+import styled from "styled-components";
 
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDnYn_55Fxu5meXkjNN6awzJTZwb3-fwbI",
+    authDomain: "lahacksteam-86084.firebaseapp.com",
+    databaseURL: "https://lahacksteam-86084.firebaseio.com",
+    projectId: "lahacksteam-86084",
+    storageBucket: "lahacksteam-86084.appspot.com",
+    messagingSenderId: "182070089013",
+    appId: "1:182070089013:web:278daaeb65cec49e8a287c",
+    measurementId: "G-4YT56H5DCW"
+  });
+}
 
-export const FavoritePage= () => {
+const firestore = firebase.firestore();
+
+const databaseRef = firebase.database().ref();
+
+export const todosRef = databaseRef.child("favorites");
+
+export const addTodo = newTodo => {
+  todosRef.push().set(newTodo);
+};
+
+const snapshotToArray = snapshot => {
+  const newArray = [];
+  snapshot.forEach(doc => {
+    newArray.push(doc.val());
+  });
+  return newArray;
+};
+
+export const FavoritePage = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    todosRef.on("value", snapshot => {
+      console.log(snapshot);
+      setFavorites(snapshotToArray(snapshot));
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(favorites);
+  }, [favorites]);
+
   return (
     <GalleryCard class="store">
+      {favorites.map(favorite => (
+        <>
+          <GalleryCard.Image src={zion} />
+          <GalleryCard.Footer>
+            <GalleryCard.Details
+              fullName={favorite["businessName"]}
+              dateString="3 Toilet Paper in stock"
+            />
+          </GalleryCard.Footer>
+        </>
+      ))}
       <GalleryCard.Image src={zion} />
       <GalleryCard.Footer>
         <GalleryCard.Details
@@ -51,7 +108,10 @@ export const FavoritePage= () => {
         />
       </GalleryCard.Footer>
     </GalleryCard>
-
   );
-}
+};
 
+const NewlyAdded = styled.div`
+  background-color: #ffdc00;
+  padding: 5px;
+`;
--- src/components/pages/MapPage.js
+++ src/components/pages/MapPage.js
@@ -15,10 +15,39 @@ import {
 import { myjson } from "../../data/hospital.js";
 import { myjson_two } from "../../data/store.js";
 
+import firebase from "firebase";
+import { makeid } from "./helpers";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDnYn_55Fxu5meXkjNN6awzJTZwb3-fwbI",
+    authDomain: "lahacksteam-86084.firebaseapp.com",
+    databaseURL: "https://lahacksteam-86084.firebaseio.com",
+    projectId: "lahacksteam-86084",
+    storageBucket: "lahacksteam-86084.appspot.com",
+    messagingSenderId: "182070089013",
+    appId: "1:182070089013:web:278daaeb65cec49e8a287c",
+    measurementId: "G-4YT56H5DCW"
+  });
+}
+
+const databaseRef = firebase.database().ref();
+
+export const todosRef = databaseRef.child("favorites");
+
+export const addTodo = newTodo => {
+  todosRef.push().set(newTodo);
+};
+
 function Map() {
   var selectedChoice = localStorage.getItem("selectedChoice");
   const [selectedPark, setSelectedPark] = useState(null);
   const [selectedH, setSelectedH] = useState(null);
+  const [selectedS, setSelectedS] = useState(null);
+
+  const isFavorite = store => {
+    myjson_two.map(s => s["LOCATION ACCOUNT #"]).includes(store["favoriteId"]);
+  };
 
   return (
     <GoogleMap
@@ -36,6 +65,9 @@ function Map() {
               }}
               onClick={() => {
                 setSelectedH(medical);
+              }}
+              onCloseClick={() => {
+                setSelectedH(null);
               }}
               icon={{
                 url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
@@ -61,14 +93,50 @@ function Map() {
         </InfoWindow>
       )}
 
+      {selectedS && selectedS?.LOCATION && (
+        <InfoWindow
+          position={{
+            lat: selectedS.LOCATION[0],
+            lng: selectedS.LOCATION[1]
+          }}
+          onCloseClick={() => {
+            setSelectedS(null);
+          }}
+        >
+          <div>
+            <h2>
+              {isFavorite(selectedS) ? "(Favorite) " : ""}
+              {selectedS["BUSINESS NAME"]}
+            </h2>
+            <p>{selectedS["NAICS"]}</p>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={e => {
+                e.preventDefault();
+                addTodo({
+                  favoriteId: selectedS["LOCATION ACCOUNT #"],
+                  businessName: selectedS["BUSINESS NAME"]
+                });
+              }}
+            >
+              Add to favorites
+            </Button>
+          </div>
+        </InfoWindow>
+      )}
+
       {myjson_two.map(
         store =>
           selectedChoice === "toilet paper" && (
             <Marker
-              key={`store-${store["LOCATION ACCOUNT #"]}`}
+              key={makeid()}
               position={{
                 lat: store.LOCATION[0],
                 lng: store.LOCATION[1]
+              }}
+              onClick={() => {
+                setSelectedS(store);
               }}
               icon={{
                 url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
@@ -81,18 +149,25 @@ function Map() {
 }
 
 export const MapPage = () => {
+  const [open, setOpen] = useState(true);
   const WrappedMap = withScriptjs(withGoogleMap(Map));
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <WrappedMap
-      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=
+    <>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=
   ${process.env.REACT_APP_GOOGLE_KEY}`}
-      loadingElement={<CircularProgress variant="indeterminate" />}
-      // loadingElement={<div style={{ height: "100vh" }} />}
-      containerElement={<LoadingContainer />}
-      // containerElement={<div style={{ height: "100vh" }} />}
-      mapElement={<div style={{ height: "100vh" }} />}
-    />
+        loadingElement={<CircularProgress variant="indeterminate" />}
+        loadingElement={<div style={{ height: "100vh" }} />}
+        containerElement={<LoadingContainer />}
+        // containerElement={<div style={{ height: "100vh" }} />}
+        mapElement={<div style={{ height: "100vh" }} />}
+      />
+    </>
   );
 };
 
